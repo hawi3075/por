ef464eea69846536b8d04a494e16cc26e17ac9b6
hawi3075 <hawig3266@gmail.com>
2025-05-08 17:37:05 +0300

    Add files via upload

---
+++ assets/images/images.ts
@@ -0,0 +1,15 @@
+import { ImageSourcePropType } from 'react-native';
+
+const Robot: ImageSourcePropType = require('./Robot.png');
+const Google: ImageSourcePropType = require('./Google.png');
+const Apple: ImageSourcePropType = require('./Apple.png');
+const Microsoft: ImageSourcePropType = require('./Microsoft-removebg-preview.png');
+
+const images = {
+  Robot,
+  Google,
+  Apple,
+  Microsoft,
+};
+
+export default images;
